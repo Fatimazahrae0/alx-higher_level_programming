b7530db8cf40359a15d5bf1da960c6b756fc0e5c
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-08-10 21:06:44 +0100

    Done

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+from sys import argv
+if len(argv) == 1:
+    print("0 arguments.")
+else:
+    print("{} arguments:".format(len(argv) - 1))
+    for i in range(len(argv) - 1):
+        print("{} : {}".format(i + 1, argv[i + 1]))
