a71f269d1272c28360f6c691bd136d06bbe0921a
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-08-10 21:15:41 +0100

    Done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -5,6 +5,6 @@ import sys
 if len(sys.argv) == 0:
     print("0 arguments.")
 else:
-    print("{} arguments:".format(len(sys.argv) - 1))
+    print("{:d} arguments:".format(len(sys.argv) - 1))
 for i in range(len(sys.argv) - 1):
-    print("{} : {}".format(i + 1, sys.argv[i + 1]))
+    print("{:d} : {:s}".format(i + 1, sys.argv[i + 1]))
