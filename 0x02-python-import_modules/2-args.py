693b5e6d5ebdcd50e20f5ef1c25266c34edd2af1
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-08-10 21:08:14 +0100

    Done

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+if __name__ == "__main__":
+    """print the list of element"""
 from sys import argv
 if len(argv) == 1:
     print("0 arguments.")
 else:
     print("{} arguments:".format(len(argv) - 1))
-    for i in range(len(argv) - 1):
-        print("{} : {}".format(i + 1, argv[i + 1]))
+for i in range(len(argv) - 1):
+    print("{} : {}".format(i + 1, argv[i + 1]))
