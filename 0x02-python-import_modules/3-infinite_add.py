112495ac06a0987ddd6b4beef561df852699d710
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-08-11 21:57:41 +0100

    Done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-length = len(argv)
-if length == 1:
-    print(0)
-elif length > 1:
-    total = 0
-    for i in range(1, length):
-        total += int(argv[i])
-    print("{:d}".format(total))
+if __name__ == "__main__":
+    
+    length = len(argv)
+    if length == 1:
+        print(0)
+    elif length > 1:
+        total = 0
+        for i in range(1, length):
+            total += int(argv[i])
+        print("{:d}".format(total))
