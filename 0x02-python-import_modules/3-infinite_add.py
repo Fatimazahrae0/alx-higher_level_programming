432a2e160467cf0641e9b4598f00d32ca3697237
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-08-11 21:59:34 +0100

    Done

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
-from sys import argv
+
 if __name__ == "__main__":
-    
-    length = len(argv)
-    if length == 1:
-        print(0)
-    elif length > 1:
-        total = 0
-        for i in range(1, length):
-            total += int(argv[i])
-        print("{:d}".format(total))
+    """Infinite add all the arguments"""
+    import sys
+
+    sum = 0
+    for i in range(1, len(sys.argv)):
+        sum += int(sys.argv[i])
+    print("{}".format(sum))
