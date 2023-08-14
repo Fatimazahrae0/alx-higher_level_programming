d789e2dfe59606c82f8875ef2ee5d697a7259df3
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-08-14 19:15:05 +0100

    Done

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    for row in matrix:
+        for i in row:
+            print("{}",format(i),
+                  end="" if row[len(row)-1] == i else " " )
+            print("".format())
