c744f535ac17e112377929356dc7d28e2355e9f4
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-08-29 23:12:39 +0100

    Done

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -16,10 +16,11 @@ class Square:
 
     def area(self):
         return self.__size ** 2
-    
+
     @property
     def size(self):
         return self.__size
+
     @size.setter
     def size(self, size):
         if type(size) == int:
