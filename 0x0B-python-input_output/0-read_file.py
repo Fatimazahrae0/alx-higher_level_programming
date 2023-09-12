22bb901e4653468afba814fbea3b09d92fbcd530
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-09-13 00:13:31 +0100

    Done

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
-"""code"""
+"""
+    0-read_file: read_file()
+"""
 
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding='utf-8') as file:
-        print(file.read())
+    """
+        read_file reads teaxt file and prints to stdout
+    """
+    with open(filename, "r", encoding='utf-8') as a_file:
+        print("{}".format(a_file.read()), end="")
