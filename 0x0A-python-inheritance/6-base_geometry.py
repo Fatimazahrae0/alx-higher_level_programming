3b1c428bf8cf58eb04008fadf9533f0febb62165
fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-09-14 18:19:52 +0100

    Done

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""class Gemotry"""
+
+
+class BaseGeometry:
+    """define a Public instance method"""
+    def area(self):
+        raise Exception("area() is not implemented")
