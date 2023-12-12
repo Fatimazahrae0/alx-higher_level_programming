23becb1ce2de7ce0c5edfc354536cabcc87d50b5
Fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-12-12 19:46:34 +0100

    done

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,6 +1,6 @@
 -- create a table in the current databass in mysql server
---description : first_table
---id : INT
---name: VARCHAR(256)
+-- description : first_table
+-- id : INT
+-- name: VARCHAR(256)
 
 CREATE TABLE IF NOT EXISTS first_table (id INT, name VARCHAR(256));
