e896859ada6b148a5c262d82e3597ade2a7cebb3
Fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-12-13 22:02:07 +0100

    done

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,2 @@
+-- Create database and table.
+CREATE TABLE IF NOT EXISTS `hbtn_0d_usa`.`cities`(id INT NOT NULL UNIQUE PRIMARY KEY AUTO_INCREMENT, state_id INT NOT NULL, FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id), name VARCHAR(256) NOT NULL);
