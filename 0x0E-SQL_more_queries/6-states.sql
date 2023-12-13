9f9bbfcfcc579ffc4a6fac82b8d2470d11bb0368
Fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2023-12-13 20:45:09 +0100

    done

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,3 +1,3 @@
 -- create a database and the table.
 CREATE DATABASE if NOT EXISTS hbtn_0d_usa;
-CREATE Table IF NOT EXISTS `hbtn_0d_usa`.`states`(id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
+CREATE Table IF NOT EXISTS `hbtn_0d_usa`.`states`(PRIMARY KEY(id), id INT NOT NULL AUTO_INCREMENT, name VARCHAR(256) NOT NULL);
