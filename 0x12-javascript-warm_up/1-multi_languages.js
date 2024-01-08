814bb38d3b1374c8cbf0495bf5870a6695418ff2
Fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2024-01-08 23:58:50 +0100

    done

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,2 +1,7 @@
 #!/usr/bin/node
-let first
+let firstLine = 'C is fun';
+let secondLine = 'Python is cool';
+let thirdLine = 'JavaScript is amazing';
+console.log(firstLine);
+console.log(secondLine);
+console.log(thirdLine);
