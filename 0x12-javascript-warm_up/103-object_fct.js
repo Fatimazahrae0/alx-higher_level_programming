c9f180c82281f067a4f7d8fc240a03211cb9d68f
Fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2024-01-15 16:36:10 +0100

    done

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -3,10 +3,13 @@ const myObject = {
   type: 'object',
   value: 12
 };
-console.log(myObject);
+
+// Define the incr function
 myObject.incr = function () {
   this.value++;
 };
+
+console.log(myObject);
 myObject.incr();
 console.log(myObject);
 myObject.incr();
