68af797afece867a33651a475f0569229994f248
Fatimazahrae0 <ouadrassifatimazahrae7@gmail.com>
2024-01-15 16:38:32 +0100

    done

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,8 +1,30 @@
 #!/usr/bin/node
-if (!process.argv[2] || !process.argv[3]) {
-  console.log(0);
-} else {
-  const args = process.argv.slice(2);
-  const intArgs = args.sort((a, b) => a - b);
-  console.log(intArgs[intArgs.length - 2]);
+
+// Function to find the second biggest integer
+function secondBiggest(arr) {
+  if (arr.length <= 2) {
+    return 0;
+  }
+
+  let first = parseInt(arr[2]);
+  let second = parseInt(arr[3]);
+
+  for (let i = 2; i < arr.length; i++) {
+    let num = parseInt(arr[i]);
+
+    if (num > first) {
+      second = first;
+      first = num;
+    } else if (num > second && num < first) {
+      second = num;
+    }
+  }
+
+  return second;
 }
+
+// Parse command line arguments
+const args = process.argv.slice(2);
+
+// Call the function and print the result
+console.log(secondBiggest(args));
